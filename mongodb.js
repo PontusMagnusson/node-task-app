282809a49da33cbee097a900780cc50a8b6ea578
MAGNUSSON Pontus <pontus.magnusson@protonmail.com>
2019-06-04 15:53:51 +0200

    Remove example code

--- mongodb.js
+++ mongodb.js
@@ -1,5 +1,4 @@
-const mongodb = require('mongodb')
-const MongoClient = mongodb.MongoClient
+const { MongoClient, ObjectID } = require('mongodb')
 
 const connectionUrl = 'mongodb://127.0.0.1:27017'
 const databaseName = 'task-manager'
@@ -12,54 +11,4 @@ MongoClient.connect(connectionUrl, { useNewUrlParser: true }, (error, client) =>
     console.log(`Connected successfully to ${connectionUrl}`)
 
     const db = client.db(databaseName)
-
-    // db.collection('users').insertOne({
-    //     name: 'Pontus',
-    //     age: 24
-    //   }, (error, result) => {
-    //     if(error) {
-    //         return console.log('Unable to insert user')
-    //     }
-
-    //     console.log(result.ops)
-    //   })
-
-    // db.collection('users').insertMany([
-    //     {
-    //         name: 'Sven',
-    //         age: 30
-    //     },
-    //     {
-    //         name: 'Jessica',
-    //         age: 26
-    //     }
-    // ], (error, result) => {
-    //     if (error) {
-    //         console.log('Failed to insert documents')
-    //     }
-
-    //     console.log(result.ops)
-    // })
-
-    db.collection('tasks').insertMany([
-        {
-            description: 'Vacuum floors',
-            completed: false
-        },
-        {
-            description: 'Iron shirts',
-            completed: true
-        },
-        {
-            description: 'Walk dog',
-            completed: false
-        }
-    ], (error, result) => {
-        if (error) {
-            return console.log('Failed to insert documents')
-        }
-
-        console.log(result.ops)
-
-    })
 })
