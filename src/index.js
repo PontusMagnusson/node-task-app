ed0bc803453edb93c3a248a30968322843409fea
MAGNUSSON Pontus <pontus.magnusson@protonmail.com>
2019-06-18 13:14:57 +0200

    Remove fallback port

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const TaskRouter = require('./routers/task')
 
 // Configure app
 const app  = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT
 app.use(express.json())
 
 // Register routers
